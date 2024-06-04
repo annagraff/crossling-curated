6745e939a4a1471586f220526653da195adf2bf4
Taras Zakharko <taras.zakharko@uzh.ch>
2024-06-04 07:28:43 +0200

    Added a script that combines all build phases

--- scripts/build-curated-datasets.R
+++ scripts/build-curated-datasets.R
@@ -4,5 +4,14 @@ if(!file.exists("scripts/build-curated-datasets.R")) rlang::abort(
 )
 
 
-cli::cli_h1("Building GBInd")
-source("scripts/GBInd/build-datasets.R")
+cat("Building curated GBInd datasets\n")
+source("scripts/GBInd/create-full-datasets.R")
+source("scripts/GBInd/densify-datasets.R")
+source("scripts/GBInd/test-dependencies.R")
+
+
+cat("Building curated TypLinkInd datasets\n")
+source("scripts/TypLinkInd/compile-external-input.R")
+source("scripts/TypLinkInd/create-full-datasets.R")
+source("scripts/TypLinkInd/densify-datasets.R")
+source("scripts/TypLinkInd/test-dependencies.R")
